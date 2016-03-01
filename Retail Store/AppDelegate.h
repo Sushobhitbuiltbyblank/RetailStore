5eb856673f243d88261df06b60e3083ced5ae254
Sushobhitbuiltbyblank <sushobhit.builtbyblank@gmail.com>
2016-03-01 11:07:02 +0530

    Initial Commit

---
+++ Retail Store/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Retail Store
+//
+//  Created by Sushobhit_BuiltByBlank on 3/1/16.
+//  Copyright © 2016 Sushobhit Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Retail Store/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Retail Store
+//
+//  Created by Sushobhit_BuiltByBlank on 3/1/16.
+//  Copyright © 2016 Sushobhit Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
